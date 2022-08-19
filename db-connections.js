d853c6e4006737ec380ffd7d192e75204f4612cb
githubsoap <88619951+githubsoap@users.noreply.github.com>
2022-08-19 16:35:17 +0800

    Update db-connections.js

--- db-connections.js
+++ db-connections.js
@@ -1,11 +1,9 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     host:'ctec-project.cvck7uksgddo.ap-southeast-1.rds.amazonaws.com',
-    port: 3306,
     user: 'admin',
     password: 'CTECpassword1!',
     database:'gaming_review',
-    connectTimeout: 30000
 });
 
 connection.connect(err => {
